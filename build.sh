c6f4d8ecdf2a9e697fc05aff96c4ed6987c0d6d4
Vlad <vlad@v-lad.org>
2021-03-24 21:46:22 -0700

    Local tweaks
    Local tweaks
    
    More local tweaks
    
    Support centos
    
    Local tweaks
    
    Use my own role

--- build.sh
+++ build.sh
@@ -45,8 +45,8 @@ source $build_conf
 [[ -z "$vm_memory" ]] && die_var_unset "vm_memory"
 [[ -z "$proxmox_host" ]] && die_var_unset "proxmox_host"
 [[ -z "$default_vm_id" ]] && die_var_unset "default_vm_id"
-[[ -z "$iso_url" ]] && die_var_unset "iso_url"
-[[ -z "$iso_sha256_url" ]] && die_var_unset "iso_sha256_url"
+#[[ -z "$iso_url" ]] && die_var_unset "iso_url"
+#[[ -z "$iso_sha256_url" ]] && die_var_unset "iso_sha256_url"
 [[ -z "$iso_directory" ]] && die_var_unset "iso_directory"
 
 ## check that build-mode (proxmox|debug) is passed to script
@@ -87,15 +87,15 @@ fi
 
 ## download ISO and Ansible role
 printf "\n=> Downloading and checking ISO\n\n"
-iso_filename=$(basename $iso_url)
-wget -P $iso_directory -N $iso_url                  # only re-download when newer on the server
-wget --no-verbose $iso_sha256_url -O $iso_directory/SHA256SUMS  # always download and overwrite
-(cd $iso_directory && cat $iso_directory/SHA256SUMS | grep $iso_filename | sha256sum --check)
-if [ $? -eq 1 ]; then echo "ISO checksum does not match!"; exit 1; fi
+#iso_filename=$(basename $iso_url)
+#wget -P $iso_directory -N $iso_url                  # only re-download when newer on the server
+#wget --no-verbose $iso_sha256_url -O $iso_directory/SHA256SUMS  # always download and overwrite
+#(cd $iso_directory && cat $iso_directory/SHA256SUMS | grep $iso_filename | sha256sum --check)
+#if [ $? -eq 1 ]; then echo "ISO checksum does not match!"; exit 1; fi
 
 printf "\n=> Downloading Ansible role\n\n"
 # will always overwrite role to get latest version from Github
-ansible-galaxy install --force -p playbook/roles -r playbook/requirements.yml
+#ansible-galaxy install --force -p playbook/roles -r playbook/requirements.yml
 [[ -f playbook/roles/ansible-initial-server/tasks/main.yml ]] || { echo "Ansible role not found."; exit 1; }
 
 mkdir -p http
